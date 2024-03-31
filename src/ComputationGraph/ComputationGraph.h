71d6b036ae02e287b239f58f51f22e6a0e2bb13a
dRamee <maksim.yolckin@yandex.com>
2024-03-31 16:40:11 +0700

    Formating

--- src/ComputationGraph/ComputationGraph.h
+++ src/ComputationGraph/ComputationGraph.h
@@ -1,4 +1,16 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+
 namespace NSTTF
 {
+    class ComputatitonGraph
+    {
+        std::vector<InputNode> input;
+        std::vector<OutputNode> output;
 
+    public:
+        ComputatitonGraph() = default;
+    };
 }
---
+++ src/utils/OperationDict.h
@@ -0,0 +1,9 @@
+#include <map>
+#include <iostream>
+#include <string>
+
+class Operations
+{
+private:
+    const std::map<const std::string, const void *> opers;
+};
