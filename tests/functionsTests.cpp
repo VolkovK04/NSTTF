82619014824dffc0ac8668e3ac1a8c6e02fdbcc7
VolkovK04 <Volkov_Kirill2004@mail.ru>
2024-04-14 16:56:12 +0300

    add example test

--- tests/functionsTests.cpp
+++ tests/functionsTests.cpp
@@ -1,30 +1,42 @@
+#include "gtest/gtest.h"
+
+#include <CL/cl.h>
+
+#include <libgpu/context.h>
+#include <libgpu/shared_device_buffer.h>
+#include <libutils/misc.h>
+
 #include "../src/tensor/tensor.h"
 #include "../src/utils/functions.h"
-#include "gtest/gtest.h"
+
 #include <vector>
 
-
 using namespace NSTTF;
+class OperationTests : public ::testing::Test {
+protected:
+gpu::Context context;
+  virtual void SetUp() {
+    // Initialize OpenCL context, command queue, and other resources
+    // This code is specific to your OpenCL setup and platform
 
-TEST(SumTest, WrongSize1) {
-    Tensor a(std::vector<float>{3});
-    Tensor b(std::vector<float>{5, 6});
+    std::vector<gpu::Device> devices = gpu::enumDevices();
 
-    std::vector<Tensor> tensors;
-    tensors.push_back(a);
-    tensors.push_back(b);
+    gpu::Device device = devices[devices.size() - 1];
 
-    EXPECT_THROW({
-        sum(tensors);
-    }, std::runtime_error);
+
+    context.init(device.device_id_opencl);
+    context.activate();
+  }
+};
+
+TEST_F(OperationTests, PositiveSumTest) {
+  Tensor a(std::vector<float>{1, 2, 3});
+  Tensor b(std::vector<float>{4, 5, 6});
+  Tensor c = sum({a, b});
+  gpu::gpu_mem_32f buff = c.getGPUBuffer();
+  std::vector<float> v(3);
+  buff.readN(v.data(), 3);
+  EXPECT_EQ(v[0], 5);
+  EXPECT_EQ(v[1], 7);
+  EXPECT_EQ(v[2], 9);
 }
-
-// TEST(SumTest, WrongSize2) {
-//     Tensor a(std::vector<float>{2, 3});
-//     Tensor b(std::vector<float>{1, 5, 6});
-// }
-
-// TEST(SumTest, WrongSize3) {
-//     Tensor a(std::vector<float>{2, 3});
-//     Tensor b(std::vector<float>{5, 6});
-// }
