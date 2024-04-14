89badd6e1f3b48bf748fcafc2d911b1f4b41485e
dRamee <maksim.yolckin@yandex.com>
2024-04-14 23:34:17 +0700

    add functions to tensor and testCoverage

--- src/tensor/gpuPointer.cpp
+++ src/tensor/gpuPointer.cpp
@@ -4,20 +4,18 @@
 
 namespace NSTTF {
 
-GPUPointer::GPUPointer(gpu::gpu_mem_32f& pointer) : pointer(pointer) {}
-GPUPointer::GPUPointer(const std::vector<float>& vector) {
-    pointer.resizeN(vector.size());
-    pointer.writeN(vector.data(), vector.size());
+GPUPointer::GPUPointer(gpu::gpu_mem_32f &pointer) : pointer(pointer) {}
+GPUPointer::GPUPointer(const std::vector<float> &vector) {
+  pointer.resizeN(vector.size());
+  pointer.writeN(vector.data(), vector.size());
 }
-GPUPointer::GPUPointer(size_t size) {
-    pointer.resizeN(size);
+GPUPointer::GPUPointer(size_t size) { pointer.resizeN(size); }
+
+std::vector<float> GPUPointer::toVector() const {
+  std::vector<float> vector;
+  pointer.readN(vector.data(), pointer.size());
+  return vector;
 }
 
-std::vector<float> GPUPointer::toVector() {
-    std::vector<float> vector;
-    pointer.readN(vector.data(), pointer.size());
-    return vector;
-}
-
-gpu::gpu_mem_32f GPUPointer::toGPUBuffer() { return pointer; }
+gpu::gpu_mem_32f GPUPointer::toGPUBuffer() const { return pointer; }
 } // namespace NSTTF
--- src/tensor/ramPointer.cpp
+++ src/tensor/ramPointer.cpp
@@ -2,15 +2,15 @@
 
 namespace NSTTF {
 
-RAMPointer::RAMPointer(std::vector<float>& pointer) : pointer(pointer){};
+RAMPointer::RAMPointer(std::vector<float> &pointer) : pointer(pointer){};
 
 std::vector<float> RAMPointer::toVector() { return pointer; }
 
 gpu::gpu_mem_32f RAMPointer::toGPUBuffer() {
-    gpu::gpu_mem_32f buffer;
-    buffer.resizeN(pointer.size());
-    buffer.writeN(pointer.data(), pointer.size());
-    return buffer;
+  gpu::gpu_mem_32f buffer;
+  buffer.resizeN(pointer.size());
+  buffer.writeN(pointer.data(), pointer.size());
+  return buffer;
 }
 
 } // namespace NSTTF
