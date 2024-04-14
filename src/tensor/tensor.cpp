7b09fde434d3352974fe07bc969f44ff567adb4f
dRamee <maksim.yolckin@yandex.com>
2024-04-14 23:53:36 +0700

    little fixes

--- src/tensor/tensor.cpp
+++ src/tensor/tensor.cpp
@@ -14,18 +14,6 @@ Tensor::Tensor(const std::vector<float> &data, const std::vector<size_t> &shape)
   }
 }
 
-gpu::gpu_mem_32f Tensor::getGPUBuffer() const { return pointer.toGPUBuffer(); }
-
-Tensor::Tensor(const std::vector<float> &data, const std::vector<size_t> &shape)
-    : shape(shape), pointer(data) {
-  size_t size = getSize();
-  if (size == 0) {
-    throw std::invalid_argument("Invalid shape");
-  }
-  if (data.size() != size) {
-    throw std::invalid_argument("Invalid data");
-  }
-}
 
 Tensor::Tensor(const std::vector<size_t> &shape)
     : shape(shape), pointer(getSize(shape)) {
@@ -46,15 +34,6 @@ size_t Tensor::getSize() const {
     size *= dim;
   }
   return size;
-size_t Tensor::getSize() const {
-  if (shape.size() == 0) {
-    return 0;
-  }
-  size_t size = 1;
-  for (size_t dim : shape) {
-    size *= dim;
-  }
-  return size;
 }
 
 size_t Tensor::getSize(const std::vector<size_t> &shape) {
@@ -63,26 +42,6 @@ size_t Tensor::getSize(const std::vector<size_t> &shape) {
     size *= dim;
   }
   return size;
-  size_t size = 1;
-  for (size_t dim : shape) {
-    size *= dim;
-  }
-  return size;
-}
-
-std::vector<float> Tensor::getData() const {
-  size_t size = getSize(getShape());
-  gpu::gpu_mem_32f buffer = getGPUBuffer();
-  std::vector<float> data(size);
-  buffer.readN(data.data(), size);
-  return data;
-}
-
-void Tensor::reshape(const std::vector<size_t> &newShape) {
-  if (getSize(newShape) != getSize()) {
-    throw std::invalid_argument("Incompatible shapes");
-  }
-  shape = newShape;
 }
 
 std::vector<float> Tensor::getData() const {
