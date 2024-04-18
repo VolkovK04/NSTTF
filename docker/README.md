4a0582e87878557a5547a64efa51a664b81db235
VolkovK04 <Volkov_Kirill2004@mail.ru>
2024-04-18 21:20:23 +0300

    add docker

---
+++ docker/README.md
@@ -0,0 +1,13 @@
+
+Build docker image for testing environment.
+
+```
+docker build -t nsttf/nsttf:1.0 -f Dockerfile .
+```
+
+Launch testing environment.
+
+```
+docker run -it --rm --name=nsttf --mount type=bind,source="${PWD}/..",target=/src nsttf/nsttf:1.0 bash 
+```
+
--- src/cl_functions/cl_functions.h
+++ src/cl_functions/cl_functions.h
@@ -1,5 +1,5 @@
 #pragma once
-#include <cstdint>
+#include <cstddef>
 
 extern const char matrix_multiplication_kernel[];
 extern const char matrix_transpose_kernel[];
@@ -7,8 +7,8 @@ extern const char multiplication_kernel[];
 extern const char subtraction_kernel[];
 extern const char sum_kernel[];
 
-extern std::size_t matrix_multiplication_kernel_length;
-extern std::size_t matrix_transpose_kernel_length;
-extern std::size_t multiplication_kernel_length;
-extern std::size_t subtraction_kernel_length;
-extern std::size_t sum_kernel_length;
+extern size_t matrix_multiplication_kernel_length;
+extern size_t matrix_transpose_kernel_length;
+extern size_t multiplication_kernel_length;
+extern size_t subtraction_kernel_length;
+extern size_t sum_kernel_length;
