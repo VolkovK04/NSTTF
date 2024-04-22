caf168409d73a629d2ef7e4063c0284ceb2b0ad2
VolkovK04 <Volkov_Kirill2004@mail.ru>
2024-04-22 23:23:11 +0300

    some changes

---
+++ clang-format.cmake
@@ -0,0 +1,18 @@
+file(GLOB_RECURSE
+     ALL_CXX_SOURCE_FILES
+     *.[chi]pp *.[chi]xx *.cc *.hh *.ii *.[CHI]
+     )
+
+# Adding clang-format target if executable is found
+find_program(CLANG_FORMAT "clang-format")
+if(CLANG_FORMAT)
+  add_custom_target(
+    clang-format
+    COMMAND /usr/bin/clang-format
+    -i
+    -style=file
+    ${ALL_CXX_SOURCE_FILES}
+    )
+else()
+    message(WARNING "clang-format not found")
+endif()
--- src/main.cpp
+++ src/main.cpp
@@ -1,55 +1,26 @@
-
-// #include "utils/compiler.h"
 #include "tensor/tensor.h"
 #include "utils/functions.h"
 #include <CL/cl.h>
 #include <libgpu/context.h>
 #include <libgpu/shared_device_buffer.h>
-
-// #include <iostream>
 #include <libutils/misc.h>
 #include <vector>
-
-// #include <map>
 
 using namespace NSTTF;
 
 gpu::Context context;
+
+
+
+
 gpu::Device device;
 
-int main(int argc, char **argv) {
-  // auto g = NSTTF::ComputationGraph();
+int main() 
+{
 
-  // auto a = g.AddInputNode("a");
-  // auto b = g.AddInputNode("b");
-  // NSTTF::AbstractOperation oper("sum");
-
-  // auto c = g.AddOperationNode(oper, {&a, &b}, "c", true);
-
-  // NSTTF::GraphExecutor executor = NSTTF::Compiler().compile(g);
-
-  // NSTTF::Tensor tensor1(std::vector<float>{1, 2, 3});
-  // NSTTF::Tensor tensor2(std::vector<float>{4, 5, 6});
-
-  // std::map<std::string, NSTTF::Tensor> tensors = {{"a", tensor1}, {"b",
-  // tensor2}};
-
-  // executor.execute(tensors);
-  std::vector<gpu::Device> devices = gpu::enumDevices();
-
-  device = devices[devices.size() - 1];
+  std::vector<  gpu::Device> devices =              gpu::enumDevices();
+  device = devices[devices.size()-1];
   std::cout << device.name << std::endl;
 
-  context.init(device.device_id_opencl);
-  context.activate();
-
-  Tensor a(std::vector<float>{1, 2, 3});
-  Tensor b(std::vector<float>{4, 5, 6});
-  Tensor c = sum({a, b});
-  gpu::gpu_mem_32f buff = c.getGPUBuffer();
-  std::vector<float> v(3);
-  buff.readN(v.data(), 3);
-  std::cout << v[0] << v[1] << v[2] << std::endl;
-  // std::cout << "Hello world" << std::endl;
   return 0;
 }
