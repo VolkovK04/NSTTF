3fd768f4a6cfeec09a73eaf213e428aa5e58bcec
Isn0v <isnovikov0@gmail.com>
2024-04-26 22:38:22 +0700

    first version of operations with derivatives

--- src/operations/abstractOperation.cpp
+++ src/operations/abstractOperation.cpp
@@ -1,6 +1,104 @@
 #include "abstractOperation.h"
 
-namespace NSTTF {
-const std::string AbstractOperation::getName() { return name; }
-AbstractOperation::AbstractOperation(std::string name) : name(name) {}
+namespace NSTTF
+{
+    const std::string AbstractOperation::getName() { return name; }
+    AbstractOperation::AbstractOperation(std::string name) : name(name) {}
+    UnaryOperation::UnaryOperation(std::string name, std::shared_ptr<Expression> expression) : expression(expression), AbstractOperation(name)
+    {
+    }
+    bool UnaryOperation::equals(const Expression &expression) const
+    {
+        return this->expression->equals(expression);
+    }
+    BinaryOperation::BinaryOperation(std::string name, const Expression &left, const Expression &right) : left(left), right(right), AbstractOperation(name)
+    {
+    }
+    bool BinaryOperation::equals(const Expression &expression) const
+    {
+        return this->left->equals(expression) && this->right->equals(expression);
+    }
+    Variable::Variable(std::string name) : AbstractOperation(name)
+    {
+    }
+
+    Expression Variable::getDerivative(const Expression &expression)
+    {
+        if (*this == expression)
+        {
+            return Constant(1);
+        }
+    }
+
+    std::string Variable::getName() const
+    {
+        return std::move(name);
+    }
+
+    bool Variable::equals(const Expression &expression) const
+    {
+        const Variable *var = dynamic_cast<const Variable *>(&expression);
+        if (var)
+        {
+            return this->getName() == var->getName();
+        }
+        return false;
+    }
+
+    bool Expression::operator==(const Expression &other)
+    {
+        return this->equals(other);
+    }
+
+    bool Expression::operator!=(const Expression &other)
+    {
+        return !(this->equals(other));
+    }
+
+    Constant::Constant(size_t value) : value(value), AbstractOperation(std::to_string(value))
+    {
+    }
+
+    Expression Constant::getDerivative(const Expression &expression)
+    {
+        return Constant(0);
+    }
+
+    bool Constant::equals(const Expression &expression) const
+    {
+        const Constant *constant = dynamic_cast<const Constant *>(&expression);
+        if (constant)
+        {
+            return this->value == constant->value;
+        }
+        return false;
+    }
+
+    Sum::Sum(std::string name, const Expression &left, const Expression &right) : left(left), right(right), AbstractOperation(name)
+    {
+    }
+
+    Expression Sum::getDerivative(const Expression &expression)
+    {
+        return Sum("Sum", left.getDerivative(expression), right.getDerivative(expression));
+    }
+
+    Subtraction::Subtraction(std::string name, const Expression &left, const Expression &right) : left(left), right(right), AbstractOperation(name)
+    {
+    }
+
+    Expression Subtraction::getDerivative(const Expression &expression)
+    {
+        return Subtraction("Subtraction", left.getDerivative(expression), right.getDerivative(expression));
+    }
+
+    Multiplication::Multiplication(std::string name, const Expression &left, const Expression &right) : left(left), right(right), AbstractOperation(name)
+    {
+    }
+
+    Expression Multiplication::getDerivative(const Expression &expression)
+    {
+        return Sum("Sum", Multiplication("Multiplication", left.getDerivative(expression), right), Multiplication("Multiplication", left, right.getDerivative(expression)));
+    }
+
 } // namespace NSTTF
--- src/operations/abstractOperation.h
+++ src/operations/abstractOperation.h
@@ -1,19 +1,127 @@
 #pragma once
 
 #include <string>
-namespace NSTTF {
+#include <tensor/tensor.h>
 
-class AbstractOperation {
+namespace NSTTF
+{
 
-private:
-  std::string name;
+  class Expression
+  {
 
-public:
-  const std::string getName();
+  public:
+    Expression() = default;
+    virtual Expression getDerivative(const Expression &expression) = 0;
+    virtual ~Expression() = default;
 
-  AbstractOperation() = default;
-  AbstractOperation(std::string name);
-  virtual ~AbstractOperation() = default;
-};
+    // AbstractNode asNode() const;
+    // идея в том, чтобы получить дифференциал и кастануть его в ноду или во что-то иное
+
+    virtual bool equals(const Expression &expression) const = 0;
+
+    virtual bool operator==(const Expression &other);
+    virtual bool operator!=(const Expression &other);
+  };
+
+  class AbstractOperation
+  {
+
+  protected:
+    std::string name;
+
+  public:
+    const std::string getName();
+
+    AbstractOperation() = default;
+    AbstractOperation(std::string name);
+
+    virtual ~AbstractOperation() = default;
+  };
+
+  class UnaryOperation : public Expression, public AbstractOperation
+  {
+
+  private:
+    std::shared_ptr<Expression> expression;
+
+  public:
+    UnaryOperation(std::string name, std::shared_ptr<Expression> expression);
+
+    bool equals(const Expression &expression) const override;
+  };
+
+  class BinaryOperation : public Expression, public AbstractOperation
+  {
+
+  protected:
+    const Expression &left;
+    const Expression &right;
+
+  public:
+    BinaryOperation(std::string name, const Expression &left, const Expression &right);
+
+    bool equals(const Expression &expression) const override;
+  };
+
+  class Variable : public Expression, public AbstractOperation
+  {
+
+  public:
+    Variable(std::string name);
+    Expression getDerivative(const Expression &expression) override;
+    std::string getName() const;
+
+    bool equals(const Expression &expression) const override;
+  };
+
+  class Constant : public Expression, public AbstractOperation
+  {
+
+  private:
+    size_t value; // broadcasting возможно бахнуть
+
+  public:
+    Constant(size_t value);
+    Expression getDerivative(const Expression &expression) override;
+    bool equals(const Expression &expression) const override;
+  };
+
+  class Sum : public AbstractOperation, public BinaryOperation
+  {
+
+  public:
+    Sum(std::string name, const Expression &left, const Expression &right);
+    Expression getDerivative(const Expression &expression) override;
+  };
+
+  class Subtraction : public AbstractOperation, public BinaryOperation
+  {
+  public:
+    Subtraction(std::string name, const Expression &left, const Expression &right);
+    Expression getDerivative(const Expression &expression) override;
+  };
+
+  class Multiplication : public AbstractOperation, public BinaryOperation
+  {
+  public:
+    Multiplication(std::string name, const Expression &left, const Expression &right);
+    Expression getDerivative(const Expression &expression) override;
+  };
+
+  class Division : public AbstractOperation, public BinaryOperation
+  {
+  public:
+    // Можно не надо?
+    // Division(std::string name, const Expression& left, const Expression& right);
+    // Expression getDerivative(const Expression& expression) override;
+  };
+
+  class Exponent : public AbstractOperation, public BinaryOperation
+  {
+  public:
+    // Можно не надо?
+    // Exponent(std::string name, const Expression& left, const Expression& right);
+    // Expression getDerivative(const Expression& expression) override;
+  };
 
 } // namespace NSTTF
